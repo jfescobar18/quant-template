17df7a9838d44c902e3962df592f4159803ceec9
jfescobar18 <jfescobar18@hotmail.com>
2025-04-04 12:23:17 -0600

    init story

---
+++ src/stories/ChartContainer.stories.js
@@ -0,0 +1,10 @@
+import ChartContainer from './ChartContainer.vue'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+export default {
+  title: 'Example/ChartContainer',
+  component: ChartContainer,
+  tags: ['autodocs']
+}
+
+export const Default = {};
---
+++ src/stories/ChartContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>ChartContainer</h1>
+</template>
+
+<script>
+import './chartContainer.css'
+
+export default {
+  name: 'chart-container'
+}
+</script>
