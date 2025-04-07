95e1161ba2485a2a005f1fed737cfaf159c5c539
jfescobar18 <jfescobar18@hotmail.com>
2025-04-07 08:27:29 -0600

    create container timeline

---
+++ src/layouts/Timeline.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="timeline-container pt-[24px] pb-[24px] pl-[24px] pr-[24px]">
+    <div class="flex flex-col gap-[4px]">
+      <TimelineStep
+        v-for="(item, index) in data.data"
+        :key="index"
+        :step="item.step"
+        :state="item.state"
+        :isLast="index === data.data.length - 1"
+      />
+    </div>
+  </div>
+</template>
+<script setup>
+import TimelineStep from '@/components/ui/TimelineStep/TimelineStep.vue'
+
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+    default: () => ({
+      data: [
+        { step: 'Created', state: 'complete' },
+        { step: 'Action Agent', state: 'inProgress' },
+        { step: 'Escalated to Human', state: 'incomplete' }
+      ]
+    })
+  }
+})
+</script>
+<style scoped>
+.timeline-container {
+  width: 465px;
+  background-color: #121213;
+  border-radius: 12px;
+  color: #f7f7f7;
+}
+</style>
