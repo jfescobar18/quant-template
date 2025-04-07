4926b9bfb67a09633451bfa9ad9b1ce8e99ce4fc
jfescobar18 <jfescobar18@hotmail.com>
2025-04-07 07:26:29 -0600

    add elements to container

---
+++ src/layouts/InteractionControls.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="interaction-container pt-[24px] pl-[24px] pb-[24px] pr-[24px]">
+    <InteractionHeader />
+    <InteractionButtons />
+  </div>
+</template>
+<script setup>
+import InteractionHeader from '@/components/ui/InteractionControls/InteractionHeader.vue'
+import InteractionButtons from '@/components/ui/InteractionControls/InteractionButtons.vue'
+</script>
+<style scoped>
+.interaction-container {
+  width: 465px;
+  background-color: #121213;
+  border-radius: 12px;
+  color: #f7f7f7;
+}
+</style>
